7c9dbb2d6467dad47167baba0ae3dabaa670c59d
Vicente Daie Pinilla <vdaiep@gmail.com>
2022-09-26 16:52:23 -0300

    none i think

--- iaaxblock/public/js/iaaxblock_instructor.js
+++ iaaxblock/public/js/iaaxblock_instructor.js
@@ -1,2 +1,8 @@
-function IterativeAssessedActivityStudent(runtime, element, settings) {
+function IterativeAssessedActivityInstructor(runtime, element, settings) {
+
+
+    $(element).find('.feedback-button').on('click', function (eventObject) {
+        eventObject.preventDefault();
+        console.log("hola");
+    });
 }
